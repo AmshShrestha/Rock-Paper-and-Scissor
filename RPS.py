219dd1cd2df39eb2876b234f67625bff27062bea
AmshShrestha <sthaamsh37@gmail.com>
2021-09-29 20:38:28 +0545

    inserted image in tkinter

--- RPS.py
+++ RPS.py
@@ -1,6 +1,17 @@
 from tkinter import*
+from random import randint
+
 win=Tk()
 win.iconbitmap("")
 win.title("rps.png")
-win.geometry("600x300")
+win.geometry("600x500")
+
+#Deine images
+rock=PhotoImage(file="Rock.png")
+paper=PhotoImage(file="Paper.png")
+scissor=PhotoImage(file="Scissor.png")
+
+
+
+
 win.mainloop()
