329b9b8d8a9a6522b416b6b6384c0a7365357398
AmshShrestha <sthaamsh37@gmail.com>
2021-10-01 10:56:29 +0545

    fixed ttk error

--- RPS.py
+++ RPS.py
@@ -1,10 +1,11 @@
 from tkinter import*
 from random import randint
+from tkinter import ttk
 
 win=Tk()
 win.iconbitmap("")
 win.title("rps.png")
-win.geometry("600x500")
+
 
 #Deine images
 rock=PhotoImage(file="Rock.png")
@@ -24,6 +25,11 @@ image_label.pack(pady=20)
 def spin():
     number_pick = randint(0, 2)
     image_label.config(win, image=image_list[number_pick], bd=0)
+
+#make our choice
+user_choice = ttk. Combobox(win, value =("Rock", "Paper", "Scissor"))
+user_choice.current=(0)
+user_choice.pack(pady=20)
 
 
 #Create spin Button
