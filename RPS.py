3c907c96fc9e905879ededbc8a25b5a4407dba28
AmshShrestha <sthaamsh37@gmail.com>
2021-10-01 11:41:22 +0545

    wrote other code

--- RPS.py
+++ RPS.py
@@ -13,27 +13,65 @@ paper=PhotoImage(file="Paper.png")
 scissor=PhotoImage(file="Scissor.png")
 
 #Add images to a list
-image_list=[rock,paper, scissor]
+image_list=[rock, paper, scissor]
 
 #Random number between 0 and 2
 number_pick=randint(0,2)
 
 #Throw up an image when the program starts
-image_label=Label(win, image=image_list[number_pick])
+image_label=Label(win, image=image_list[number_pick], bd=0)
 image_label.pack(pady=20)
 
 def spin():
     number_pick = randint(0, 2)
-    image_label.config(win, image=image_list[number_pick], bd=0)
+
+    #show image
+    image_label.config(image=image_list[number_pick])
+
+    #convert drop down choice to a number
+    if user_choice.get()=="rock":
+        user_choice_value = 0
+    elif user_choice.get()=="paper":
+        user_choice_value = 1
+    elif user_choice.get() == "scissor":
+        user_choice_value = 2
+
+    # Determine if we won or lost
+    if user_choice_value==0: #rock
+         if number_pick==0:
+            win_lose_label.config(text="Tie! Spin Again..")
+         elif number_pick==1: #paper
+             win_lose_label.config(text="Lose! Paper Beats Rock")
+         elif number_pick==2: #scissor
+             win_lose_label.config(text="Win! Rock Breaks Scissor")
+
+    if user_choice_value == 1:  # paper
+        if number_pick == 0:
+            win_lose_label.config(text="Win! Paper Beats Rock")
+        elif number_pick == 1:  # paper
+            win_lose_label.config(text="Tie! Spin Again")
+        elif number_pick == 2:  # scissor
+            win_lose_label.config(text="Lose! Scissor Cuts Paper")
+            
+    if user_choice_value == 2:  # scissor
+        if number_pick == 2:#rock
+            win_lose_label.config(text="Lose! Rock Breaks Scissor")
+        elif number_pick == 1:  # paper
+            win_lose_label.config(text="Win! Scissor Cuts Paper ")
+        elif number_pick == 2:  # scissor
+            win_lose_label.config(text="Tie! Spin Again")
 
 #make our choice
-user_choice = ttk. Combobox(win, value =("Rock", "Paper", "Scissor"))
+user_choice = ttk.Combobox(win, value =("Rock", "Paper", "Scissor"))
 user_choice.current=(0)
 user_choice.pack(pady=20)
 
 
 #Create spin Button
 spin_button=Button(win, text="Spin!", command=spin)
+spin_button.pack(pady=10)
 
+win_lose_label = Label(win, text="", font="helvetica" "18")
+win_lose_label.pack(pady=50)
 
 win.mainloop()
