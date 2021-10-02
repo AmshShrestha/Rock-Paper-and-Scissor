f36e8617d409245640357a8cca7dc8d37a3241f6
AmshShrestha <sthaamsh37@gmail.com>
2021-10-02 09:25:22 +0545

    finished framing and texting in frame_1

--- rockpaperscissor.py
+++ rockpaperscissor.py
@@ -15,7 +15,20 @@ Label(win, text="Rock Paper Scissor", font="normal 20 bold", fg="blue").pack(pad
 frame=Frame(win)
 frame.pack()
 
+l1=Label(frame, text="PLAYER          ", font=10)
+l1.pack(side=LEFT)
 
+l2=Label(frame, text="VS          ", font= "10")
+l2.pack(side=LEFT)
+
+l3=Label(frame, text="COMPUTER          ", font="10")
+l3.pack()
+
+l4=Label(win, text="", font="20", bg="white", width=15, borderwidth= 2, relief="solid")
+l4.pack(pady=20)
+
+frame_1=Frame(win)
+frame_1.pack()
 
 
 win.mainloop()
